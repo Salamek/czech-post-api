ca5662290abeb59a20b9870cd2fe42b6ce63170e
Jan Herzan <jan.herzan@gmail.com>
2020-04-18 10:51:22 +0200

    Package as letter introduced

--- src/Enum/Product.php
+++ src/Enum/Product.php
@@ -8,12 +8,14 @@ namespace Salamek\CzechPostApi\Enum;
 
 class Product
 {
+    const PACKAGE_AS_LETTER = 'RR';
     const PACKAGE_TO_HAND = 'DR';
     const PACKAGE_TO_THE_POST_OFFICE = 'NP';
 
     /** @var array */
     public static $list = [
         self::PACKAGE_TO_HAND,
-        self::PACKAGE_TO_THE_POST_OFFICE
+        self::PACKAGE_TO_THE_POST_OFFICE,
+        self::PACKAGE_AS_LETTER
     ];
 }
